8a898ede58d942391099649520dc6598638b3308
Luca <luca@192.168.1.9>
2026-06-17 15:31:49 +0200

    for practice

---
+++ gen_qr.py
@@ -0,0 +1,11 @@
+import qrcode
+
+answer = input("Enter the data to encode in the QR code:  ")
+
+img = qrcode.make(answer)
+
+img.save("qrcode.png")
+
+img.show()
+
+print("QR code generated and saved as 'qrcode.png")
